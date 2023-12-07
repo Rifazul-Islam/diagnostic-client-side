0f5959778ad6ee5cda9490c953f8cc41a8ba6841
Rifazul <rifazul50@gmail.com>
2023-12-07 22:16:37 +0600

    add new commit

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,21 +1,19 @@
-
 import Banner from "../Banner/Banner";
 import Doctor from "../Doctor/Doctor";
 import Featured from "../Featured/Featured";
 import Recommendation from "../Recommendation/Recommendation";
 
-
 const Home = () => {
-    return (
-        <div>
-          <Banner></Banner>
-          <Featured></Featured>
-          <Recommendation></Recommendation>
+  return (
+    <div>
+      <h1> Hello Brother</h1>
+      <Banner></Banner>
+      <Featured></Featured>
+      <Recommendation></Recommendation>
 
-          <Doctor></Doctor>
-          
-        </div>
-    );
+      <Doctor></Doctor>
+    </div>
+  );
 };
 
 export default Home;
